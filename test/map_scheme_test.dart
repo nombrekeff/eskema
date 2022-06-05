55d0f71712e9793685c99c9f33aa633bb13639ff
Manolo edge Edge <nombrekeff@rallf.com>
2022-06-05 22:18:51 +0200

    test: organized tests

--- test/map_scheme_test.dart
+++ test/map_scheme_test.dart
@@ -23,6 +23,7 @@ void main() {
     final invalidRes3 = field.validate({'name': 'test', 'age': -12});
     expect(invalidRes3.isValid, false);
     expect(invalidRes3.expected, 'age -> higher or equal 0');
+    expect(invalidRes3.toString(), 'Expected age -> higher or equal 0');
 
     final validRes1 = field.validate({'name': 'test', 'age': 12});
     expect(validRes1.isValid, true);
@@ -63,5 +64,31 @@ void main() {
     });
     expect(invalidRes5.isValid, false);
     expect(invalidRes5.expected, 'address -> additional -> doorbel_number -> int');
+  });
+  
+  test('Map with ListScheme', () {
+    final field = MapScheme({
+      'books': ListScheme(
+        fieldValidator: MapScheme({
+          'name': Field([isTypeString()]),
+        }),
+      ),
+    });
+
+    final validRes1 = field.validate({'books': []});
+    expect(validRes1.isValid, true);
+
+    final validRes2 = field.validate({
+      'books': [
+        {'name': 'bookname'}
+      ],
+    });
+    expect(validRes2.isValid, true);
+
+    final invalidRes1 = field.validate({
+      'books': [{}]
+    });
+    expect(invalidRes1.isValid, false);
+    expect(invalidRes1.expected, 'books -> [0] -> name -> String');
   });
 }
