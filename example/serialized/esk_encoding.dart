130f1a2c8cd9415429234a056df77f407cae5012
manolo edge <manoloedge@manolos-MacBook-Pro.local>
2026-02-27 22:28:33 +0100

    docs: Update `esk_encoding` example to demonstrate `stringLength` validator with a custom message and delete `test_message_serialize.dart`.

--- example/serialized/esk_encoding.dart
+++ example/serialized/esk_encoding.dart
@@ -1,32 +1,14 @@
 import 'package:eskema/eskema.dart';
 
-// void main() {
-//   final validator = all([
-//     isGt(0),
-//     isLt(100),
-//     isType<int>(),
-//   ]);
-
-//   final encoded = const EskemaEncoder().encode(validator);
-//   final decodedValidator = const EskemaDecoder().decode(encoded);
-
-//   print(encoded); // (>0 & <100 & int)
-
-//   print(decodedValidator.validate(50).isValid);   // true
-//   print(decodedValidator.validate(100).isValid);  // false
-//   print(decodedValidator.validate(0).isValid);    // false
-//   print(decodedValidator.validate('50').isValid); // false
-// }
-
 void main() {
   final validator = all([
-    stringLength([isGt(0), isLt(100)]),
+    stringLength([isGt(0), isLt(100)], message: 'String length must be between 0 and 100'),
   ]);
 
   final encoded = const EskemaEncoder().encode(validator);
   final decodedValidator = const EskemaDecoder().decode(encoded);
 
-  print(encoded); // (slen(>0, <100))
+  print(encoded); // (slen(>0, <100, "String length must be between 0 and 100"))
 
   print(decodedValidator.validate('50').isValid); // true
   print(decodedValidator.validate('').isValid); // false
