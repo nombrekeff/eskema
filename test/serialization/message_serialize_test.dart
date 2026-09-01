56fb231c0f3245fc6ed540f91e9a7b46d46dd4f2
manolo edge <manoloedge@manolos-MacBook-Pro.local>
2026-09-01 19:27:05 +0200

    refactor: use single quotes in tests and update pubspec dependencies

--- test/serialization/message_serialize_test.dart
+++ test/serialization/message_serialize_test.dart
@@ -3,12 +3,12 @@ import 'package:eskema/eskema.dart';
 
 void main() {
   test('serialize message', () {
-    final val1 = isString(message: "Must be a string");
+    final val1 = isString(message: 'Must be a string');
     
     final eskemaEncoder = EskemaEncoder();
     final jsonEncoder = JsonEncoder();
     
-    print("Eskema format: ${eskemaEncoder.encode(val1)}");
-    print("JSON format: ${jsonEncoder.encode(val1)}");
+    print('Eskema format: ${eskemaEncoder.encode(val1)}');
+    print('JSON format: ${jsonEncoder.encode(val1)}');
   });
 }
